068d43d9cb3476590201fd619458b4e706aeda1f
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-21 21:57:56 -0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import type { Component } from 'solid-js';
+import { createSignal, createEffect } from "solid-js"
 
 const App: Component = () => {
+  const [count, setCount] = createSignal(0)
+  const increment = () => setCount((prev) => prev + 1)
+
+  createEffect(() => {
+    console.log(count());
+  });
+
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <>
+      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind! {count()}</p>
+      <button onclick={increment}>Increment</button>
+    </>
   );
 };
 
