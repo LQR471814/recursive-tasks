d21f3c8ba31ed0434ac27e2794ee83398fb660e3
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-01 19:00:45 -0800

    add sleep data script

---
+++ schema/sleep.ts
@@ -0,0 +1,84 @@
+import "temporal-polyfill/global";
+
+import { and, eq, gte, type InferInsertModel, lt } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { ROOT_ID } from "src/lib/constants";
+import { childInstancesOf, day, year } from "../src/lib/timescales";
+import { taskTable } from "./schema";
+
+const sql = postgres(
+	process.env.DB_URL ??
+		"postgresql://postgres:postgres@127.0.0.1:54322/postgres",
+);
+const db = drizzle({ client: sql });
+
+async function main() {
+	// add sleep tasks for this year
+	const now = Temporal.Now.zonedDateTimeISO();
+
+	const thisYear = year.instance(now);
+
+	await db
+		.delete(taskTable)
+		.where(
+			and(
+				eq(taskTable.name, "Sleep (year)"),
+				eq(taskTable.timeframe_start, thisYear.start.epochMilliseconds),
+			),
+		);
+	const yearTaskId = crypto.randomUUID();
+	await db.insert(taskTable).values([
+		{
+			id: yearTaskId,
+			name: "Sleep (year)",
+			comments: "",
+			optimistic: 1,
+			expected: 1,
+			pessimistic: 1,
+			implementation: "children",
+			parent_id: ROOT_ID,
+			assigned_to: null,
+			timeframe_start: thisYear.start.epochMilliseconds,
+			timescale: "year",
+		},
+	]);
+
+	// days
+	await db
+		.delete(taskTable)
+		.where(
+			and(
+				eq(taskTable.name, "Sleep"),
+				gte(taskTable.timeframe_start, thisYear.start.epochMilliseconds),
+				lt(taskTable.timeframe_start, thisYear.start.epochMilliseconds),
+			),
+		);
+	const values: InferInsertModel<typeof taskTable>[] = [];
+	for (const child of childInstancesOf(year, day, now)) {
+		values.push({
+			id: crypto.randomUUID(),
+			name: "Sleep",
+			parent_id: yearTaskId,
+			assigned_to: null,
+			timeframe_start: child.epochMilliseconds,
+			timescale: "day",
+			optimistic: 9,
+			expected: 9,
+			pessimistic: 9,
+			implementation: "hours",
+			comments: "",
+		});
+	}
+	await db.insert(taskTable).values(values);
+}
+
+main()
+	.then(() => {
+		console.log("Added sleep data.");
+		process.exit(0);
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
