6b67544bebf7a92835d77dcd03afc51f6af5085b
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-01 16:52:21 -0800

    amend combobox logic

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -2,7 +2,7 @@
 
 import { createFilter } from "@kobalte/core";
 import type { Fn, Pipe, Tuples, Unions } from "hotscript";
-import { createSignal } from "solid-js";
+import { createSignal, createMemo, createEffect } from "solid-js";
 import {
 	Combobox,
 	ComboboxContent,
@@ -31,9 +31,9 @@ export function Search<
 	onChange(result: T | null): void;
 }) {
 	const filter = createFilter({ sensitivity: "base", ignorePunctuation: true });
-	const [options, setOptions] = createSignal(props.options);
+	const optionSignal = createMemo(() => createSignal(props.options));
 	const onInputChange = (value: string) => {
-		setOptions(
+		optionSignal()[1](
 			props.options.filter((option) =>
 				filter.contains(option[props.labelField as any] as any, value),
 			),
@@ -43,11 +43,14 @@ export function Search<
 		<Combobox
 			name={props.name}
 			value={props.selected}
-			options={options()}
+			options={optionSignal()[0]()}
 			optionValue={props.idField}
 			optionLabel={props.labelField as any}
 			onInputChange={onInputChange}
 			onChange={props.onChange}
+			onClick={() => {
+				onInputChange("");
+			}}
 			itemComponent={(p) => (
 				<ComboboxItem item={p.item}>
 					{p.item.rawValue[props.labelField as any] as any}
