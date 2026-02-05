41f508d4b9deafba1fbabaae2d5de4bd0e4a27f3
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-04 20:11:49 -0800

    add debounced for percentile calc

--- src/components/timeframe.tsx
+++ src/components/timeframe.tsx
@@ -24,6 +24,7 @@ import {
 import { cn } from "~/lib/utils";
 import { TaskChip } from "./task";
 import { Button } from "./ui/button";
+import { debounce } from "@tanstack/pacer";
 
 const cachedPercentiles = new Map<string, number | Promise<number>>();
 
@@ -50,47 +51,54 @@ function usePercentileDuration(
 		{ duration: number; error: null } | { duration: null; error: Error } | null
 	>(null);
 
+	const calculate = debounce(
+		(p: number) => {
+			let hash = "";
+			for (const t of tasks()) {
+				hash += `${p}|${t.optimistic}:${t.expected}:${t.pessimistic},`;
+			}
+			for (const t of dependencies()) {
+				hash += `|${t.optimistic}:${t.expected}:${t.pessimistic},`;
+			}
+			const cached = cachedPercentiles.get(hash);
+			if (typeof cached === "number") {
+				setState({ duration: cached, error: null });
+				return;
+			}
+
+			setState(null);
+
+			const promise =
+				cached instanceof Promise
+					? cached
+					: evalStats(
+							tasks().map((t) => t.id),
+							{
+								type: "percentile",
+								percentile: p,
+							},
+						);
+
+			cachedPercentiles.set(hash, promise);
+
+			promise
+				.then((dur) => {
+					setState({ duration: dur, error: null });
+					cachedPercentiles.set(hash, dur);
+				})
+				.catch((err) => {
+					console.error(err);
+					setState({ duration: null, error: err });
+				});
+		},
+		{ wait: 100 },
+	);
+
 	createEffect(() => {
 		const p = percentile();
-
-		let hash = "";
-		for (const t of tasks()) {
-			hash += `${p}|${t.optimistic}:${t.expected}:${t.pessimistic},`;
-		}
-		for (const t of dependencies()) {
-			hash += `|${t.optimistic}:${t.expected}:${t.pessimistic},`;
-		}
-		const cached = cachedPercentiles.get(hash);
-		if (typeof cached === "number") {
-			setState({ duration: cached, error: null });
-			return;
-		}
-
-		setState(null);
-
-		const promise =
-			cached instanceof Promise
-				? cached
-				: evalStats(
-						tasks().map((t) => t.id),
-						{
-							type: "percentile",
-							percentile: p,
-						},
-					);
-
-		cachedPercentiles.set(hash, promise);
-
-		promise
-			.then((dur) => {
-				setState({ duration: dur, error: null });
-				cachedPercentiles.set(hash, dur);
-			})
-			.catch((err) => {
-				console.error(err);
-				setState({ duration: null, error: err });
-			});
+		calculate(p);
 	});
+
 	return state;
 }
 
