0862641ebcbf846a687462beb6a63d497906bbf1
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-08 21:11:06 -0800

    add example env

--- src/lib/trailbase/index.ts
+++ src/lib/trailbase/index.ts
@@ -1,7 +1,7 @@
 import { initClient } from "trailbase";
 
 export const trailBaseClient = initClient(
-	import.meta.env.VITE_TRAILBASE_URL ?? "http://localhost:4000",
+	import.meta.env.VITE_TRAILBASE_URL ?? window.origin.toString(),
 );
 
 export * as executor from "./executor.gen";
