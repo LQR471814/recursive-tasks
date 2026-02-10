e7264ea83c8f339ebb27762e0b599b45fbafcbf8
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-09 19:40:04 -0800

    add status field to db

---
+++ traildepot/migrations/main/U1770694016__update.sql
@@ -0,0 +1,56 @@
+-- add the status field
+
+create table task1 (
+	id blob primary key check(is_uuid_v7(id)),
+	name text not null,
+	comments text not null,
+	status int not null,
+
+	timescale int not null,
+	timeframe_start int not null,
+
+	assigned_to blob references executor(id)
+		on update cascade
+		on delete set null,
+	parent_id blob not null references task(id)
+		on update cascade
+		on delete cascade,
+
+	optimistic real not null,
+	expected real not null,
+	pessimistic real not null,
+
+	implementation int not null
+) strict;
+
+insert into task1 (
+	status,
+	id,
+	name,
+	comments,
+	timescale,
+	timeframe_start,
+	assigned_to,
+	parent_id,
+	optimistic,
+	expected,
+	pessimistic,
+	implementation
+)
+select
+	0 as status,
+	id,
+	name,
+	comments,
+	timescale,
+	timeframe_start,
+	assigned_to,
+	parent_id,
+	optimistic,
+	expected,
+	pessimistic,
+	implementation
+from task;
+
+drop table task;
+alter table task1 rename to task;
